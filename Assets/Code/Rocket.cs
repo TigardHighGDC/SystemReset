824bc3ea675024c2bf6390c4c09347b7775d2f14
JakeSmig <95725136+JakeSmig@users.noreply.github.com>
2022-01-20 15:50:32 -0800

    added test explosion
    funny haha

--- Assets/Code/Rocket.cs
+++ Assets/Code/Rocket.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 { 
+
+    public float radius = 5.0F;
+    public float power = 10.0F;
+
     void OnTriggerEnter(Collider collide)
     {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+         foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+        }
+        
         Destroy(gameObject);
     }
 
