7028d59c0381d2fe6bc2dc1877105c4dfa41f756
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2022-03-10 16:02:26 -0800

    Almost there

--- Assets/Code/AnkleBiter/AnkleBiterControl.cs
+++ Assets/Code/AnkleBiter/AnkleBiterControl.cs
@@ -6,39 +6,46 @@ using System.Text;
 public class AnkleBiterControl : MonoBehaviour
 {
     public GameObject player;
-    public int ankleBiter;
-    Vector3[] positions;
-    List<Vector3> bitersArr = new List<Vector3>();
     // Start is called before the first frame update
-    float total = 1000000000000000.0f;
 
     public void ReCalc()
     {
+        float total;
+        int ankleBiter = 0;
+        float[] positions;
         ankleBiter = 0;
+        List<Vector3> bitersArr = new List<Vector3>();
         foreach (GameObject biter in GameObject.FindGameObjectsWithTag("ankleBiter"))
         {
             bitersArr.Add(biter.transform.position);
             ankleBiter += 1;
         }
-        positions = new Vector3[ankleBiter];
+        positions = new float[ankleBiter];
 
         for (int i = 0; i < ankleBiter; i++)
         {
             float angle = i * Mathf.PI*2f / ankleBiter;
-            positions[i] = player.transform.position + new Vector3(Mathf.Cos(angle)*100, 0.0f, Mathf.Sin(angle)*100);
+            positions[i] = angle;
         }
+
         float amount = 0.0f;
         int[] enemiesPos = new int[ankleBiter];
+        float line = 0.0f;
+        float line2 = 0.0f;
         for (int i = 0; i < ankleBiter; i++)
         {
             total = 100000000000.0f;
             for (int u = 0; u < ankleBiter; u++)
             {
-                //Line defined by two points
-                //enemy 0
-                //positions 1
-                //player 2
-                amount = Mathf.Abs((player.transform.position[0] - positions[i][0])*(positions[i][2]-bitersArr[u][2]) - (positions[i][0]-bitersArr[u][0])*(player.transform.position[2]-positions[i][2])) / Mathf.Sqrt(Mathf.Pow(player.transform.position[0]-positions[i][0], 2) + Mathf.Pow(player.transform.position[2]-positions[i][2], 2));
+                Vector3 arcCircle = player.transform.position;
+                line = Mathf.Sqrt(Mathf.Pow(player.transform.position[0] - bitersArr[u][0], 2) + Mathf.Pow(player.transform.position[2] - bitersArr[u][2], 2));
+                arcCircle[0] += Mathf.Cos(positions[i]) * line;
+                arcCircle[2] += Mathf.Sin(positions[i]) * line;
+                line2 =  Mathf.Sqrt(Mathf.Pow(arcCircle[0] - bitersArr[u][0], 2) + Mathf.Pow(arcCircle[2] - bitersArr[u][2], 2));
+                amount = Mathf.Pow(line2, 2);
+                amount -= Mathf.Pow(line, 2) * 2;
+                amount = amount / ((-1) * Mathf.Pow(line, 2)*2);
+                amount = Mathf.Acos(amount);
                 Debug.Log(amount);
                 if (amount < total)
                 {
