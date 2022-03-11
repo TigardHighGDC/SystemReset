5ed0582823d7453e7ea22302e2d31e7003199917
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2022-03-11 14:54:46 -0800

    IT FINALLYS WORKS

--- Assets/Code/AnkleBiter/AnkleBiterControl.cs
+++ Assets/Code/AnkleBiter/AnkleBiterControl.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
+using System;
 
 public class AnkleBiterControl : MonoBehaviour
 {
@@ -14,10 +15,10 @@ public class AnkleBiterControl : MonoBehaviour
         int ankleBiter = 0;
         float[] positions;
         ankleBiter = 0;
-        List<Vector3> bitersArr = new List<Vector3>();
+        List<GameObject> bitersArr = new List<GameObject>();
         foreach (GameObject biter in GameObject.FindGameObjectsWithTag("ankleBiter"))
         {
-            bitersArr.Add(biter.transform.position);
+            bitersArr.Add(biter);
             ankleBiter += 1;
         }
         positions = new float[ankleBiter];
@@ -29,27 +30,59 @@ public class AnkleBiterControl : MonoBehaviour
         }
 
         float amount = 0.0f;
+        float[] enemiesRad = new float[ankleBiter];
         int[] enemiesPos = new int[ankleBiter];
-        float line = 0.0f;
-        float line2 = 0.0f;
+
+        float line;
+        float line2;
+        float mean = 0;
+
+        Dictionary<float, int> enemPosSort = new Dictionary<float, int>();
+        Vector3 dot;
+
         for (int i = 0; i < ankleBiter; i++)
         {
-            total = 100000000000.0f;
-            for (int u = 0; u < ankleBiter; u++)
-            {
-                Vector3 arcCircle = player.transform.position;
-                line = Mathf.Sqrt(Mathf.Pow(player.transform.position[0] - bitersArr[u][0], 2) + Mathf.Pow(player.transform.position[2] - bitersArr[u][2], 2));
-                arcCircle[0] += Mathf.Cos(positions[i]) * line;
-                arcCircle[2] += Mathf.Sin(positions[i]) * line;
-                amount =  Mathf.Sqrt(Mathf.Pow(arcCircle[0] - bitersArr[u][0], 2) + Mathf.Pow(arcCircle[2] - bitersArr[u][2], 2)) / line;
-                Debug.Log(amount);
-                if (amount < total)
-                {
-                    total = amount;
-                    enemiesPos[i] = u;
-                }
-            }    
+            //Finds radians and stores in enemiesRad
+            line = Mathf.Sqrt(Mathf.Pow(bitersArr[i].transform.position[0] - player.transform.position[0], 2) + Mathf.Pow(bitersArr[i].transform.position[2] - player.transform.position[2], 2));
+            dot = player.transform.position;
+            dot[0] += Mathf.Cos(0.0f)*line;
+            dot[2] += Mathf.Sin(0.0f)*line;
+            line2 = Mathf.Sqrt(Mathf.Pow(bitersArr[i].transform.position[0] - dot[0], 2) + Mathf.Pow(bitersArr[i].transform.position[2] - dot[2], 2));
+
+            enemiesRad[i] = Mathf.Pow(line2, 2) - Mathf.Pow(line,2) - Mathf.Pow(line,2);
+            enemiesRad[i] = enemiesRad[i] / (line * line * -2);
+            enemiesRad[i] = Mathf.Acos(enemiesRad[i]);
+            enemPosSort[enemiesRad[i]] = i;
+            mean += enemiesRad[i];
         }
+        mean = mean / ankleBiter;
+        int start = 0;
+        total = 100000.0f;
+        Debug.Log(mean);
+        Array.Sort(enemiesRad);
+        //Finds numbers closest to mean
+        for (int i = 0; i < ankleBiter; i++)
+        {
+            if (Mathf.Abs(mean - ankleBiter) < total)
+            {
+                total = Mathf.Abs(mean - ankleBiter);
+                start = i;
+            }
+        }
+        //Goes through sorted enemiesRad and stores values of enemy that would follow that side
+        int track = 0;
+        for (int i = start; i < ankleBiter; i++)
+        {
+            enemiesPos[track] = enemPosSort[enemiesRad[i]];
+            track += 1;
+
+        }
+        for (int i = 0; i < start; i++)
+        {
+            enemiesPos[track] = enemPosSort[enemiesRad[i]];
+            track += 1;
+        }
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < enemiesPos.Length; i++)
         {
