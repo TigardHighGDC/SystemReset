deac53a74201507abac31ea375058d4628b90052
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2022-03-30 22:48:40 -0700

    MagicSystem

---
+++ Assets/MagicSystem/MagicProjectile.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicProjectile : MonoBehaviour
+{
+    public float damage;
+    public float fireDamage;
+    public float homingStrength;
+    public float lightningStrength;
+    public float speed;
+    public float chaos;
+    float chaosTimer = 0.5f;
+    Vector3 direction;
+    public Rigidbody rb;
+    List<GameObject> enemiesArr = new List<GameObject>();
+    int closest = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (homingStrength > 0.0f)
+        {
+            float distance = 100000000000.0f;
+            foreach (GameObject biter in GameObject.FindGameObjectsWithTag("ankleBiter"))
+            {
+                enemiesArr.Add(biter);
+            }
+            foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("enemy"))
+            {
+                enemiesArr.Add(enemy);
+            }
+            for (int i = 0; i < enemiesArr.Count; i++)
+            {
+                if (Mathf.Sqrt(Mathf.Pow(enemiesArr[i].transform.position[0]-transform.position[0], 2) + Mathf.Pow(enemiesArr[i].transform.position[2]-transform.position[2], 2)) < distance)
+                {
+                    distance = Mathf.Sqrt(Mathf.Pow(enemiesArr[i].transform.position[0]-transform.position[0], 2) + Mathf.Pow(enemiesArr[i].transform.position[2]-transform.position[2], 2));
+                    closest = i;
+                }
+            }
+        }
+        Debug.Log("a");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (homingStrength > 0.0f)
+        {
+            direction = Vector3.RotateTowards(transform.forward, enemiesArr[closest].transform.position - transform.position, homingStrength, 0.0f)* Time.deltaTime * speed;;
+        }
+        else
+        {
+            direction = transform.forward * Time.deltaTime * speed;
+        }
+
+        
+        //direction = transform.worldToLocalMatrix.inverse * direction;
+
+        
+        rb.MovePosition(transform.position + direction);
+        chaosTimer = 0.5f;
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "enemy" || collision.tag == "ankleBiter" || collision.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/MagicSystem/MagicShoot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject spell = Instantiate(bullet, transform.position, transform.rotation);
+            spell.GetComponent<MagicProjectile>().homingStrength = 10.0f;
+        }
+        
+        
+    }
+}
