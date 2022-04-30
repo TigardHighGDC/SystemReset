a2bd27c9957231e8ec7fd20ad8404e0eb0c3c13e
Brandon <pacewicbrandon@gmail.com>
2022-04-30 11:13:09 -0700

    Update to python format script (#19)

--- format_all.py
+++ format_all.py
@@ -1,10 +1,12 @@
 import os
 
-if __name__ == '__main__':
-    # Search through all nested folders in the Assets/Code folder
-    # If the file is a .cs file format it with clang-format
+def main():
     for dirpath, _, filenames in os.walk('Assets/Code'):
         for filename in filenames:
             if filename.endswith('.cs'):
                 print(f'Trying file {dirpath}/{filename}')
                 os.system(f'clang-format -i -style=file {dirpath}/{filename}')
+
+
+if __name__ == '__main__':
+    main()
