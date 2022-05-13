d467536dd6e5b05426ebb6b7d3d186ef68aaf7bc
JakeSmig <95725136+JakeSmig@users.noreply.github.com>
2022-05-12 21:19:43 -0700

    Various

---
+++ Assets/Code/SceneChanger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+	public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+}
