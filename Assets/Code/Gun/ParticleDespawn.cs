dff1367e4b4ddb93cf6c99785990df047a50d6ca
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2022-05-12 20:06:11 -0700

    Retexture

--- Assets/Code/Gun/ParticleDespawn.cs
+++ Assets/Code/Gun/ParticleDespawn.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ParticleDespawn : MonoBehaviour
 {
-    float timer = 1.0f;
+    float timer = 0.2f;
     // Start is called before the first frame update
 
     // Update is called once per frame
