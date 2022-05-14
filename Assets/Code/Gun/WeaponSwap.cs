7ff7714e778204f780b61073958ae2671f5b05a7
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2022-05-13 21:38:37 -0700

    WeaponSwapImprovement

--- Assets/Code/Gun/WeaponSwap.cs
+++ Assets/Code/Gun/WeaponSwap.cs
@@ -20,11 +20,11 @@ public class WeaponSwap : MonoBehaviour
 {
     public class Pistol
     {
-        public static float damage = 10.0f;
+        public static float damage = 25.0f;
         public static float timeBetweenShots = 0f;
         public static float spread = 0.10f;
         public static float reloadTime = 2.0f;
-        public static float fireRate = 1.0f;
+        public static float fireRate = 0.5f;
         public static float range = 0f;
         public static int magSize = 8;
         public static int shotsPerTap = 1;
@@ -40,7 +40,7 @@ public class WeaponSwap : MonoBehaviour
         public static float fireRate = 0.8f;
         public static float range = 25f;
         public static int magSize = 120;
-        public static int shotsPerTap = 15;
+        public static int shotsPerTap = 25;
         public static bool allowButtonHold = false;
     }
 
@@ -59,11 +59,11 @@ public class WeaponSwap : MonoBehaviour
 
     public class SniperRifle
     {
-        public static float damage = 100.0f;
-        public static float timeBetweenShots = 5.0f;
+        public static float damage = 150.0f;
+        public static float timeBetweenShots = 0.0f;
         public static float spread = 0.0f;
         public static float range = 300.0f;
-        public static float reloadTime = 3f;
+        public static float reloadTime = 2f;
         public static float fireRate = 0.0f;
         public static int magSize = 1;
         public static int shotsPerTap = 1;
@@ -72,7 +72,7 @@ public class WeaponSwap : MonoBehaviour
 
     public class AssaultRifle
     {
-        public static float damage = 25.0f;
+        public static float damage = 20.0f;
         public static float timeBetweenShots = 0.1f;
         public static float spread = 0.02f;
         public static float range = 100.0f;
@@ -86,6 +86,12 @@ public class WeaponSwap : MonoBehaviour
     public GameObject weapon;
     private basicShoot basicShoot;
 
+    public GameObject pistol;
+    public GameObject shotgun;
+    public GameObject machineGun;
+    public GameObject sniperRifle;
+    public GameObject assaultRifle;
+    
     private void Start()
     {
         basicShoot = weapon.GetComponent<basicShoot>();
@@ -95,28 +101,54 @@ public class WeaponSwap : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Alpha1))
         {
+            pistol.SetActive(true);
+            shotgun.SetActive(false);
+            machineGun.SetActive(false);
+            sniperRifle.SetActive(false);
+            assaultRifle.SetActive(false);
             SelectPistol();
         }
         else if (Input.GetKey(KeyCode.Alpha2))
         {
+            pistol.SetActive(false);
+            shotgun.SetActive(true);
+            machineGun.SetActive(false);
+            sniperRifle.SetActive(false);
+            assaultRifle.SetActive(false);
             SelectShotgun();
         }
         else if (Input.GetKey(KeyCode.Alpha3))
         {
+            pistol.SetActive(false);
+            shotgun.SetActive(false);
+            machineGun.SetActive(true);
+            sniperRifle.SetActive(false);
+            assaultRifle.SetActive(false);
             SelectMachineGun();
         }
         else if (Input.GetKey(KeyCode.Alpha4))
         {
+            pistol.SetActive(false);
+            shotgun.SetActive(false);
+            machineGun.SetActive(false);
+            sniperRifle.SetActive(true);
+            assaultRifle.SetActive(false);
             SelectSniperRifle();
         }
         else if (Input.GetKey(KeyCode.Alpha5))
         {
+            pistol.SetActive(false);
+            shotgun.SetActive(false);
+            machineGun.SetActive(false);
+            sniperRifle.SetActive(false);
+            assaultRifle.SetActive(true);
             SelectAssaultRifle();
         }
     }
 
     private void SelectPistol()
     {
+        basicShoot.damage = Pistol.damage;
         basicShoot.spread = Pistol.spread;
         basicShoot.timeBetweenShots = Pistol.timeBetweenShots;
         basicShoot.reloadTime = Pistol.reloadTime;
@@ -130,6 +162,7 @@ public class WeaponSwap : MonoBehaviour
 
     private void SelectShotgun()
     {
+        basicShoot.damage = Shotgun.damage;
         basicShoot.spread = Shotgun.spread;
         basicShoot.timeBetweenShots = Shotgun.timeBetweenShots;
         basicShoot.reloadTime = Shotgun.reloadTime;
@@ -143,6 +176,7 @@ public class WeaponSwap : MonoBehaviour
 
     private void SelectMachineGun()
     {
+        basicShoot.damage = MachineGun.damage;
         basicShoot.spread = MachineGun.spread;
         basicShoot.timeBetweenShots = MachineGun.timeBetweenShots;
         basicShoot.reloadTime = MachineGun.reloadTime;
@@ -156,6 +190,7 @@ public class WeaponSwap : MonoBehaviour
 
     private void SelectSniperRifle()
     {
+        basicShoot.damage = SniperRifle.damage;
         basicShoot.spread = SniperRifle.spread;
         basicShoot.timeBetweenShots = SniperRifle.timeBetweenShots;
         basicShoot.reloadTime = SniperRifle.reloadTime;
@@ -169,6 +204,7 @@ public class WeaponSwap : MonoBehaviour
 
     private void SelectAssaultRifle()
     {
+        basicShoot.damage = AssaultRifle.damage;
         basicShoot.spread = AssaultRifle.spread;
         basicShoot.timeBetweenShots = AssaultRifle.timeBetweenShots;
         basicShoot.reloadTime = AssaultRifle.reloadTime;
